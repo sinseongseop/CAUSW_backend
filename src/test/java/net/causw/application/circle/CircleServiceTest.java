e1e2ef503afb5d431d505478885c65e42920a28a
sinseongseop <sss02030@naver.com>
2024-07-20 14:59:47 +0900

    test: findById() 에러 단위 테스트 추가
    에러 클래스 종류, 에러코드, 에러메세지 검증을 하는 단위 테스트를 추가하였습니다.

--- src/test/java/net/causw/application/circle/CircleServiceTest.java
+++ src/test/java/net/causw/application/circle/CircleServiceTest.java
@@ -1,19 +1,20 @@
 package net.causw.application.circle;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Validator;
 import net.causw.adapter.persistence.circle.Circle;
 import net.causw.adapter.persistence.repository.*;
 import net.causw.application.dto.circle.CircleResponseDto;
+import net.causw.domain.exceptions.BadRequestException;
+import net.causw.domain.exceptions.ErrorCode;
+import net.causw.domain.model.util.MessageUtil;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 
 // 서버 오류 발생으로 인한 test 실패를 구분하기 위해, 우선 MockitoExtension으로 test 코드를 작성 후
@@ -43,20 +44,18 @@ class CircleServiceTest {
     Validator validator;
 
     Circle circle;
-    CircleResponseDto circleResponseDto;
 
     @BeforeEach
     void setUp() {
         circle = ObjectFixtures.getCircle();
-        String circleId = "testCircleID";
-        given(circleRepository.findById(circleId)).willReturn(Optional.of(circle));
     }
 
     @Test
-    @DisplayName("Id에 해당하는 cicle 찾기 test")
+    @DisplayName("findById 성공 테스트 - circleID로 찾기")
     void findById() {
         // given
         String circleId = "testCircleID";
+        given(circleRepository.findById(circleId)).willReturn(Optional.of(circle));
 
         // when
         CircleResponseDto result = circleService.findById(circleId);
@@ -69,5 +68,21 @@ class CircleServiceTest {
         assertThat(result.getIsDeleted()).isEqualTo(circle.getIsDeleted());
         assertThat(result.getLeaderId()).isEqualTo(circle.getLeader().get().getId());
     }
+
+    @Test
+    @DisplayName("findById 에러 테스트 - circle이 존재하지 않는 경우")
+    void findById_whenCircleNotFound() {
+        // given
+        String nonExistentCircleId = "nonExistentId";
+        given(circleRepository.findById(nonExistentCircleId)).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> circleService.findById(nonExistentCircleId))
+                .isInstanceOf(BadRequestException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.ROW_DOES_NOT_EXIST)
+                .hasMessage(MessageUtil.SMALL_CLUB_NOT_FOUND);
+    }
+
+
 
 }
