faa2fe8ff57571be9f5faaf66ba614d5c4836e03
sinseongseop <sss02030@naver.com>
2024-07-21 00:36:13 +0900

    test: findAll 성공/에러 테스트 케이스 작성

--- src/test/java/net/causw/application/circle/CircleServiceTest.java
+++ src/test/java/net/causw/application/circle/CircleServiceTest.java
@@ -1,19 +1,34 @@
 package net.causw.application.circle;
 
+import groovy.lang.Tuple2;
 import jakarta.validation.Validator;
 import net.causw.adapter.persistence.circle.Circle;
+import net.causw.adapter.persistence.circle.CircleMember;
 import net.causw.adapter.persistence.repository.*;
+import net.causw.adapter.persistence.user.User;
 import net.causw.application.dto.circle.CircleResponseDto;
+import net.causw.application.dto.circle.CirclesResponseDto;
+import net.causw.application.util.TestUtil;
 import net.causw.domain.exceptions.BadRequestException;
 import net.causw.domain.exceptions.ErrorCode;
+import net.causw.domain.exceptions.UnauthorizedException;
+import net.causw.domain.model.enums.CircleMemberStatus;
+import net.causw.domain.model.enums.Role;
+import net.causw.domain.model.enums.UserState;
 import net.causw.domain.model.util.MessageUtil;
+import net.causw.domain.model.util.StaticValue;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import java.util.Optional;
 
+import java.util.*;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 
@@ -44,10 +59,14 @@ class CircleServiceTest {
     Validator validator;
 
     Circle circle;
+    CircleMember circleMember;
+    User user;
 
     @BeforeEach
     void setUp() {
-        circle = ObjectFixtures.getCircle();
+        circle = ObjectFixtures.getCircle("circle1");
+        circleMember = ObjectFixtures.getCircleMember(CircleMemberStatus.MEMBER, circle);
+        user = ObjectFixtures.getUser("user1");
     }
 
     @Test
@@ -55,7 +74,9 @@ class CircleServiceTest {
     void findById() {
         // given
         String circleId = "testCircleID";
+        Long MemberCount = 2L;
         given(circleRepository.findById(circleId)).willReturn(Optional.of(circle));
+        given(circleMemberRepository.getNumMember(circleId)).willReturn(MemberCount);
 
         // when
         CircleResponseDto result = circleService.findById(circleId);
@@ -67,6 +88,8 @@ class CircleServiceTest {
         assertThat(result.getDescription()).isEqualTo(circle.getDescription());
         assertThat(result.getIsDeleted()).isEqualTo(circle.getIsDeleted());
         assertThat(result.getLeaderId()).isEqualTo(circle.getLeader().get().getId());
+        assertThat(result.getLeaderName()).isEqualTo(circle.getLeader().get().getName());
+        assertThat(result.getNumMember()).isEqualTo(MemberCount);
     }
 
     @Test
@@ -83,6 +106,129 @@ class CircleServiceTest {
                 .hasMessage(MessageUtil.SMALL_CLUB_NOT_FOUND);
     }
 
+    @Test
+    @DisplayName("findById 에러 테스트 - 삭제된 Circle ID 호출")
+    void findById_whenDeletedCircle() {
+        // Given
+        String deletedCircleId = "deletedCircleId";
+        circle = ObjectFixtures.getDeletedCirle();
+        given(circleRepository.findById(deletedCircleId)).willReturn(Optional.of(circle));
 
+        // When & Then
+        assertThatThrownBy(() -> circleService.findById(deletedCircleId))
+                .isInstanceOf(BadRequestException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.TARGET_DELETED)
+                .hasMessage("삭제된 " + StaticValue.DOMAIN_CIRCLE + " 입니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("findAll 성공 테스트 - User가 Admin/President/VicePresident 인 경우")
+    @EnumSource(value = Role.class, names = {"ADMIN", "PRESIDENT", "VICE_PRESIDENT"})
+    void findAll_whenAdminOrPresidentOrVicePresident(Role role) {
+        // Given
+        Circle circle2 = ObjectFixtures.getCircle("circle2");
+        User user = ObjectFixtures.getUser(role);
+        String userId = user.getId();
+        given(userRepository.findById(userId)).willReturn(Optional.of(user));
+        given(circleRepository.findAll()).willReturn(Arrays.asList(circle, circle2));
+        given(circleMemberRepository.findByUser_Id(userId)).willReturn(Arrays.asList());
+
+        // When
+        List<CirclesResponseDto> result = circleService.findAll(userId);
+
+        // Then
+        assertThat(result).hasSize(2);
+        assertThat(result.get(0).getName()).isEqualTo(circle.getName());
+        assertThat(result.get(1).getName()).isEqualTo(circle2.getName());
+    }
+
+    @ParameterizedTest
+    @DisplayName("findAll 성공 테스트 - 동아리 가입상태가 Member와 Await 가 섞여 있는 경우  ")
+    @EnumSource(value = Role.class, names = {"COUNCIL","LEADER_CIRCLE","COMMON"})
+    void findAll_whenNormalUser(Role role) {
+        // Given
+        List<Circle> circles = Arrays.asList(circle, ObjectFixtures.getCircle("circle2"),
+                ObjectFixtures.getCircle("circle3"));
+        List<CircleMember> circleMembers = Arrays.asList(circleMember,
+                ObjectFixtures.getCircleMember(CircleMemberStatus.MEMBER, circles.get(1)),
+                ObjectFixtures.getCircleMember(CircleMemberStatus.AWAIT, circles.get(2)));
+        User user = ObjectFixtures.getUser(role);
+        String userId = user.getId();
+        given(userRepository.findById(userId)).willReturn(Optional.of(user));
+        given(circleRepository.findAll()).willReturn(circles);
+        given(circleMemberRepository.findByUser_Id(userId)).willReturn(circleMembers);
+
+        // 메소드 안에서 Map이 CircleID를 키로 사용하기 떄문에 ID가 반드시 필요
+        for(int i=0; i < circles.toArray().length ; i++){
+            TestUtil.setId(circles.get(i),"id","circle"+i);
+        }
+
+        // When
+        List<CirclesResponseDto> result = circleService.findAll(userId);
+
+        // Then
+        assertThat(result).hasSize(3);
+        for(int i=0; i<2; i++){
+            assertThat(result.get(i).getName()).isEqualTo(circles.get(i).getName());
+            assertThat(result.get(i).getIsJoined()).isEqualTo(TRUE);
+        }
+        assertThat(result.get(2).getName()).isEqualTo(circles.get(2).getName());
+        assertThat(result.get(2).getIsJoined()).isEqualTo(FALSE);
+    }
+
+    @Test
+    @DisplayName("findAll 에러 테스트 - User가 존재하지 않는 경우")
+    void findAll_whenUserNotFound() {
+        // given
+        String nonExistentUserId = "nonExistentId";
+        given(userRepository.findById(nonExistentUserId)).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> circleService.findAll(nonExistentUserId))
+                .isInstanceOf(BadRequestException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.ROW_DOES_NOT_EXIST)
+                .hasMessage(MessageUtil.USER_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("findAll 에러 테스트 - User의 Role이 None 인 경우")
+    void findAll_whenUserIsNone() {
+        // given
+        user = ObjectFixtures.getUser(Role.NONE);
+        String userId = "roleNone";
+        given(userRepository.findById(userId)).willReturn(Optional.of(user));
+
+        // when & then
+        assertThatThrownBy(() -> circleService.findAll(userId))
+                .isInstanceOf(UnauthorizedException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.NEED_SIGN_IN)
+                .hasMessage("접근 권한이 없습니다. 다시 로그인 해주세요. 문제 반복시 관리자에게 문의해주세요.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("findAll 에러 테스트 - userState에 따른 에러 종류 확인 ")
+    @EnumSource(value = UserState.class, names = {"DROP", "INACTIVE", "AWAIT", "REJECT"})
+    void findAll_whenUserStateThrowsException(UserState state) {
+        // given
+        user = ObjectFixtures.getUser(state);
+        String userId = user.getId();
+        given(userRepository.findById(userId)).willReturn(Optional.of(user));
+
+        // 각각의 state에 해당하는 에러
+        Map<UserState, Tuple2<ErrorCode, String>> expectedErrors = Map.of(
+                UserState.DROP, new Tuple2<>(ErrorCode.BLOCKED_USER, "추방된 사용자 입니다."),
+                UserState.INACTIVE, new Tuple2<>(ErrorCode.INACTIVE_USER, "비활성화된 사용자 입니다."),
+                UserState.AWAIT, new Tuple2<>(ErrorCode.AWAITING_USER, "대기 중인 사용자 입니다."),
+                UserState.REJECT, new Tuple2<>(ErrorCode.REJECT_USER, "가입이 거절된 사용자 입니다.")
+        );
+
+        Tuple2<ErrorCode, String> expectedError = expectedErrors.get(state);
+
+        // when & then
+        assertThatThrownBy(() -> circleService.findAll(userId))
+                .isInstanceOf(UnauthorizedException.class)
+                .hasFieldOrPropertyWithValue("errorCode", expectedError.getFirst())
+                .hasMessage(expectedError.getSecond());
+    }
 
 }
--- src/test/java/net/causw/application/circle/ObjectFixtures.java
+++ src/test/java/net/causw/application/circle/ObjectFixtures.java
@@ -1,25 +1,50 @@
 package net.causw.application.circle;
 
 import net.causw.adapter.persistence.circle.Circle;
+import net.causw.adapter.persistence.circle.CircleMember;
 import net.causw.adapter.persistence.user.User;
+import net.causw.domain.model.enums.CircleMemberStatus;
 import net.causw.domain.model.enums.Role;
 import net.causw.domain.model.enums.UserState;
 import net.causw.domain.model.user.UserDomainModel;
 
 import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 public class ObjectFixtures {
 
     //User
-    public static User getUser() {
-        UserDomainModel userDomain = UserDomainModel.of("user1", "email", "name", "password", "studentId"
-                , 2021, Role.ADMIN, "profileImage", "testRefreshToken",UserState.ACTIVE);
+    public static User getUser(String userID){
+        UserDomainModel userDomain = UserDomainModel.of(userID, "email3", "name3", "password3", "studentId3"
+                , 2023, Role.COMMON, "profileImage3", "testRefreshToken3",UserState.ACTIVE);
+        return User.from(userDomain);
+    }
+
+    public static User getUser(Role role){
+        UserDomainModel userDomain = UserDomainModel.of("testID", "email3", "name3", "password3", "studentId3"
+                , 2023, role, "profileImage3", "testRefreshToken3",UserState.ACTIVE);
+        return User.from(userDomain);
+    }
+
+    public static User getUser(UserState userState){
+        UserDomainModel userDomain = UserDomainModel.of("testID", "email", "name", "password", "studentId"
+                , 2023, Role.COMMON, "profileImage", "testRefreshToken",userState);
         return User.from(userDomain);
     }
 
     // Circle
-    public static Circle getCircle() {
-        return Circle.of("testUser1","testMainImage","testDescription", FALSE, getUser());
+    public static Circle getCircle(String circleName) {
+        return Circle.of(circleName,"testMainImage","testDescription", FALSE, getUser("testID"));
     }
+
+    public static Circle getDeletedCirle(){
+        return Circle.of("deletedCircleName","testMainImage","testDescription", TRUE, getUser("testID"));
+    }
+
+    // CircleMember
+    public static CircleMember getCircleMember(CircleMemberStatus status, Circle circle){
+        return CircleMember.of(status, circle ,getUser(Role.COMMON));
+    }
+
 
 }
